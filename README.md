d5df87214aa53396eb4d62ce3e5389ee876cda46
romainprignon <pro.rprignon@gmail.com>
2022-06-17 18:11:44 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# tools
+
+> asdf .tool-versions parser
+
+## Install
+
+- from deno.land
+```
+deno run --allow-read https://deno.land/x/tools@0.1.0/mod.js
+```
+
+- fetch binary
+```
+curl -sSLO https://deno.land/x/tools@0.1.0/bin/tools
+```
+
+## Usage
+
+Usage:   tools
+  Version: 0.1.0
+
+  Description:
+
+    asdf .tool-versions parser
+
+  Options:
+
+    -h, --help     - Show this help.                            
+    -V, --version  - Show the version number for this program.  
+    --json         - json output                                
+
+  Commands:
+
+    ls                           - list all plugins  
+    version  <plugin> [version]  - get plugin version
---
+++ mod.js
@@ -0,0 +1,53 @@
+import { Command } from "cliffy";
+
+const id = (a) => a;
+const formatter = (options) => options.json ? JSON.stringify : id;
+
+const getTools = async () => {
+  try {
+    const content = await Deno.readTextFile(`${Deno.cwd()}/.tool-versions`);
+    const tools = Object.fromEntries(
+      content.split("\n").filter((s) => s !== "").map((tool) =>
+        tool.split(" ")
+      ),
+    );
+
+    return tools;
+  } catch {
+    return {};
+  }
+};
+
+const version = async (options, ...args) => {
+  const plugin = args[0];
+
+  const format = formatter(options);
+  const tools = await getTools();
+  const version = tools[plugin];
+
+  if (version) {
+    console.log(format(version));
+  }
+};
+
+const list = async (options) => {
+  const tools = await getTools();
+
+  console.log(
+    options.json ? Object.keys(tools) : Object.keys(tools).join("\n"),
+  );
+};
+
+await new Command()
+  .name("tools")
+  .version("0.1.0")
+  .description("asdf .tool-versions parser")
+  .globalOption("--json", "json output")
+  // ls command
+  .command("ls", "list all plugins")
+  .action(list)
+  // version command
+  .command("version", "get plugin version")
+  .arguments("<plugin:string> [version:string]")
+  .action(version)
+  .parse(Deno.args);
